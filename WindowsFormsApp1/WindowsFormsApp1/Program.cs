37ddc20089db50b4736655f9d3e544291ce83819
Yair <yairgaf@post.bgu.ac.il>
2022-12-22 15:34:07 +0200

    check

---
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+        public static void checkConnection()
+        {
+            SqlCommand c = new SqlCommand();
+            c.CommandText = "EXECUTE dbo.example";
+            SQL_CON SC = new SQL_CON();
+             SC.execute_query(c);
+            
+        }
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+
+
+
