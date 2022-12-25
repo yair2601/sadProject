e4786984b6a43455e790fad91ed5879f6d0bae7a
yair2601 <84125862+yair2601@users.noreply.github.com>
2022-12-25 09:02:55 +0200

    Add files via upload

---
+++ Project/Project/Project/EmployeesStatuses.cs
@@ -0,0 +1,20 @@
+﻿// File:    Title.cs
+// Author:  ranalm
+// Created: יום שני 12 מאי 2014 21:27:47
+// Purpose: Definition of Enum Title
+
+using System;
+using System.ComponentModel;
+
+namespace Project
+{
+    public enum EmployeesStatuses
+    {
+        [Description("Lay Off")] LayOff,
+        Vacation,
+        [Description("Maternuty Leave")] MaternutyLeave,
+        [Description("Paternity Leave")] PaternityLeave,
+        Work
+
+    }
+}
---
+++ Project/Project/Project/EmployeesTypes.cs
@@ -0,0 +1,20 @@
+﻿// File:    Title.cs
+// Author:  ranalm
+// Created: יום שני 12 מאי 2014 21:27:47
+// Purpose: Definition of Enum Title
+
+using System;
+using System.ComponentModel;
+
+namespace Project
+{
+    public enum EmployeesTypes
+    {
+        [Description("Business Manager")] BusinessManager,
+        Finance,
+        [Description("ogistic Manager")] LogisticManager,
+        [Description("Pickup Employee")] PickupEmployee,
+        [Description("Production Planner")] ProductionPlanner,
+
+    }
+}
